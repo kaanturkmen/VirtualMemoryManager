7dfad4353d55d383ebb58022f371f1734c35e167
kaanturkmen <kaanturkmen@protonmail.com>
2021-05-20 13:20:11 +0300

    search_tlb method is implemented.

--- part1.c
+++ part1.c
@@ -52,7 +52,11 @@ int max(int a, int b)
 
 /* Returns the physical address from TLB or -1 if not present. */
 int search_tlb(unsigned char logical_page) {
-    /* TODO */
+    for(int i = 0; i < TLB_SIZE; i++) {
+      if(tlb[i].logical == logical_page) return tlb[i].physical;
+    }
+
+    return -1;
 }
 
 /* Adds the specified mapping to the TLB, replacing the oldest mapping (FIFO replacement). */
